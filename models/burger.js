164d7aac5a3aa40314b748c36bcbc010bc370a0f
David Naimi <d88naimi@gmail.com>
2020-07-17 14:56:25 -0700

    added the models

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+const burger = {
+    all: (cb) => {
+        orm.all("burgers", (res) => {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: (newBurger, cb) => {
+        orm.create("burgers", newBurger, (res) => {
+            cb(res);
+        });
+    },
+    update: (BurgerData, criteria, cb) => {
+        orm.update("burgers", BurgerData, criteria, (res) => {
+            cb(res);
+        });
+    },
+};
+
+module.exports = burger;
